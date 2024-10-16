9b41654795817c8c7e24e94d659a39a660b757b7
Logan Costello <logancostello2022@gmail.com>
2024-10-15 18:07:00 -0700

    solved rsa mallory

--- rsa_mallory.py
+++ rsa_mallory.py
@@ -14,5 +14,21 @@ def rsa_mallory(numBits):
     c = pow(s_bob, e, n)
 
     # Mallory receives c, sends c' to Alice
+    c_prime = n
+
+    # Alice receives c', calculates s
+    phi = (p - 1) * (q - 1)
+    d = pow(e, -1, phi)
+    s = pow(c_prime, d, n)
+
+    return s
+
+    # S must be 0
+    # From here, s is hashed into a key that is used for encryption, but mallory
+    # knows s is 0, so she can calculate the key as well, intercept the encrypted
+    # message, then decrypt it with the key
+
+if __name__ == '__main__':
+    print(rsa_mallory(128))
 
 
